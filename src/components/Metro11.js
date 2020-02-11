0b0c60fe374836c6581b5f0386cf98133702b133
Terrence Wong <wongterrencew@gmail.com>
2020-02-11 11:27:00 +0100

    chore: remove more unnecessary ids

--- src/components/Metro11.js
+++ src/components/Metro11.js
@@ -5,17 +5,12 @@ const SvgMetro11 = props => (
     <style>
       {".metro-11_svg__st1{fill-rule:evenodd;clip-rule:evenodd;fill:#fff}"}
     </style>
-    <path
-      fill="none"
-      id="metro-11_svg__canvas_background"
-      d="M-1-1h22v22H-1z"
-    />
+    <path fill="none" d="M-1-1h22v22H-1z" />
     <g>
-      <g stroke="null" id="metro-11_svg__svg_1">
-        <g stroke="null" id="metro-11_svg__svg_2">
+      <g stroke="null">
+        <g stroke="null">
           <circle
             stroke="null"
-            id="metro-11_svg__svg_3"
             r={10.009}
             cy={10.009}
             cx={10.009}
@@ -25,13 +20,11 @@ const SvgMetro11 = props => (
           />
           <path
             stroke="null"
-            id="metro-11_svg__svg_4"
             d="M8.399 15.601V4.548H6.462c-.5.391-1.697 1.131-2.98 1.74l-.653.305.761 1.697.588-.305C4.59 7.79 5.7 7.202 6.07 6.898v8.681h2.328v.022z"
             className="metro-11_svg__st1"
           />
           <path
             stroke="null"
-            id="metro-11_svg__svg_5"
             d="M15.079 15.601V4.548h-1.937c-.5.391-1.697 1.131-2.98 1.74l-.675.305.761 1.697.588-.305c.413-.195 1.523-.783 1.893-1.087v8.681h2.35v.022z"
             className="metro-11_svg__st1"
           />
@@ -42,4 +35,3 @@ const SvgMetro11 = props => (
 )
 
 export default SvgMetro11
-
