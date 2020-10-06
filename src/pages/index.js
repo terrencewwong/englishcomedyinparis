76846a0451d085f4575a4f62cb16fd5b979e1013
Terrence Wong <wongterrencew@gmail.com>
2020-10-06 11:02:11 +0200

    content: remove events that are not running

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,16 +23,6 @@ export default function IndexPage() {
       {/* <Day name="Featured Events">
       </Day> */}
       <h3>Due to COVID many events are not running, but these events are!</h3>
-      <Day name="Monday">
-        <ComedyCave />
-      </Day>
-      <Day name="Tuesday">
-        <OpenAirComedy />
-        <JuicyPunchline />
-      </Day>
-      <Day name="Wednesday">
-        <ComedyLabWednesday />
-      </Day>
       <Day name="Saturday">
         <WildOpenMic />
         <GreatBritish />
@@ -40,7 +30,6 @@ export default function IndexPage() {
       <Day name="Sunday">
         <NewYorkComedyNight />
         <UnstableComedy />
-        <ComedyFunhouse />
       </Day>
     </Layout>
   )
