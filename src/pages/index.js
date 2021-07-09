c4e9b105fee293bea4dd6e6de2bbcf5b78c572ff
Terrence Wong <wongterrencew@gmail.com>
2021-07-09 11:05:39 +0200

    add show

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,6 +30,9 @@ export default function IndexPage() {
       <Day name="Sun Jul 11 @ 7:00pm - 8:30pm">
         <NewYorkComedyNight eventLink="https://www.facebook.com/events/173591731341718/" />
       </Day>
+      <Day name="Tue Jul 13 @ 7:30pm - 8:30pm">
+        <DestinationStandUp eventLink="https://www.facebook.com/events/234445784779434/" />
+      </Day>
       <Day name="Sat Jul 17 @ 7:00pm - 8:00pm">
         <GreatBritish />
       </Day>
